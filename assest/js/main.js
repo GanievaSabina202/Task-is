8bf588de5936464fd4fc731d5a0942b7011f84bc
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2022-03-24 13:56:22 +0400

    Maine marged

---
+++ assest/js/main.js
@@ -0,0 +1,254 @@
+
+
+
+
+//Back to Top
+
+$(document).ready(function() {
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn(200);
+        } else {
+            $('.back-to-top').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    })
+});
+
+
+
+$(".circle_percent").each(function () {
+    var $this = $(this),
+        $dataV = $this.data("percent"),
+        $dataDeg = $dataV * 3.6,
+        $round = $this.find(".round_per");
+    $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+    $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+    $this.prop('Counter', 0).animate({ Counter: $dataV },
+        {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $this.find(".percent_text").text(Math.ceil(now) + "%");
+            }
+        });
+    if ($dataV >= 51) {
+        $round.css("transform", "rotate(" + 360 + "deg)");
+        setTimeout(function () {
+            $this.addClass("percent_more");
+        }, 1000);
+        setTimeout(function () {
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+        }, 1000);
+    }
+});
+
+(function ($) {
+    $.fn.countTo = function (options) {
+        options = options || {};
+
+        return $(this).each(function () {
+            // set options for current element
+            var settings = $.extend({}, $.fn.countTo.defaults, {
+                from: $(this).data('from'),
+                to: $(this).data('to'),
+                speed: $(this).data('speed'),
+                refreshInterval: $(this).data('refresh-interval'),
+                decimals: $(this).data('decimals')
+            }, options);
+
+
+            var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                increment = (settings.to - settings.from) / loops;
+
+
+            var self = this,
+                $self = $(this),
+                loopCount = 0,
+                value = settings.from,
+                data = $self.data('countTo') || {};
+
+            $self.data('countTo', data);
+
+            // if an existing interval can be found, clear it first
+            if (data.interval) {
+                clearInterval(data.interval);
+            }
+            data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+            // initialize the element with the starting value
+            render(value);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+
+                render(value);
+
+                if (typeof (settings.onUpdate) == 'function') {
+                    settings.onUpdate.call(self, value);
+                }
+
+                if (loopCount >= loops) {
+                    // remove the interval
+                    $self.removeData('countTo');
+                    clearInterval(data.interval);
+                    value = settings.to;
+
+                    if (typeof (settings.onComplete) == 'function') {
+                        settings.onComplete.call(self, value);
+                    }
+                }
+            }
+
+            function render(value) {
+                var formattedValue = settings.formatter.call(self, value, settings);
+                $self.html(formattedValue);
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,
+        to: 0,
+        speed: 1000,
+        refreshInterval: 100,
+        decimals: 0,
+        formatter: formatter,
+        onUpdate: null,
+        onComplete: null
+    };
+
+    function formatter(value, settings) {
+        return value.toFixed(settings.decimals);
+    }
+}(jQuery));
+
+jQuery(function ($) {
+    // custom formatting example
+    $('.count-number').data('countToOptions', {
+        formatter: function (value, options) {
+            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+        }
+    });
+
+
+    $('.timer').each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+});
+
+
+$(document).ready(function () {
+    $('.your-class').slick({
+        prevArrow: '<i class="fas fa-chevron-left left"></i>',
+        nextArrow: '<i class="fas fa-chevron-right right"></i>',
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 968,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+
+
+});
+
+AOS.init({
+    offset: 120,
+    delay: 120,
+    easing: 'ease',
+    duration: 1200,
+    disable: false,
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+    startEvent: 'DOMContentLoaded',
+    animatedClassName: 'aos-animate',
+    initClassName: 'aos-init',
+    useClassNames: false,
+    disableMutationObserver: false,
+    throttleDelay: 99,
+    debounceDelay: 50
+  });
+
+$('.event-slider').slick({
+    autoplay: true,
+    speed: 800,
+    lazyLoad: 'progressive',
+    arrows: true,
+    dots: false,
+    prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+    nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+}).slickAnimation();
+
+
+
+$('.slick-nav').on('click touch', function (e) {
+
+    e.preventDefault();
+
+    var arrow = $(this);
+
+    if (!arrow.hasClass('animate')) {
+        arrow.addClass('animate');
+        setTimeout(() => {
+            arrow.removeClass('animate');
+        }, 1600);
+    }
+
+});
+
+
+//Sticky Navbar addd
+const nav = document.querySelector('#nav');
+let navTop = nav.offsetTop;
+
+
+function fixedNav() {
+    if (window.scrollY >= 250) {
+        nav.classList.add('SlickNav');
+    
+    } else {
+        nav.classList.remove('SlickNav');
+    }
+}
+
+window.addEventListener('scroll', fixedNav);
